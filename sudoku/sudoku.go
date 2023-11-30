7d5a7105c614acc9d81ef8051b93d82e6f03d426
Sandy Cash <lhcash@us.ibm.com>
2023-11-30 11:25:25 -0500

    Flesh out some of the early constraint code

--- sudoku/sudoku.go
+++ sudoku/sudoku.go
@@ -1,27 +1,50 @@
 package sudoku
 
 import (
+	"fmt"
 	"math"
 )
 
 type Sudoku struct {
 	SubsetSize int
 	sizeFactor int
-	values     []int
+	values     []*GridSquare
 }
 
-type Subset []int
-
+// New returns a Sudoku with all squares set to 0, any square
+// can assume any value.  The overall Sudoku is (size^2) x (size^2) -
+// in other words, it is a sizexsize grid of sizexsize subgrids.
 func New(size int) *Sudoku {
 	subsetSize := int(math.Pow(float64(size), 2))
 
-	return &Sudoku{
+	s := &Sudoku{
 		SubsetSize: subsetSize,
 		sizeFactor: size,
-		values:     make([]int, subsetSize*subsetSize),
+		values:     make([]*GridSquare, subsetSize*subsetSize),
 	}
+
+	for i := range s.values {
+		s.values[i] = &GridSquare{}
+
+		// Initialize the row, column, and subgrid indices
+		j := i / subsetSize
+		k := i % size
+		s.values[i].RowIndex = j
+		s.values[i].ColumnIndex = k
+		s.values[i].SubgridIndex = SubgridIndex(j, k, size)
+
+		// Initialize each grid square so that it could potentially contain any value
+		s.values[i].PossibleValues = make(map[int]struct{})
+		for pv := 0; pv < subsetSize; pv++ {
+			s.values[i].PossibleValues[pv] = struct{}{}
+		}
+	}
+
+	return s
 }
 
+// NewFromRows returns a Sudoku with the values set, any unset
+// square has its candidates constrained.
 func NewFromRows(size int, rows [][]int) *Sudoku {
 	s := New(size)
 	if len(rows) != s.SubsetSize {
@@ -34,23 +57,53 @@ func NewFromRows(size int, rows [][]int) *Sudoku {
 		}
 
 		for k, val := range row {
-			s.values[(j*s.SubsetSize)+k] = val
+			s.values[(j*s.SubsetSize)+k].Value = val
 		}
 	}
 
 	return s
 }
 
-func (s *Sudoku) Set(j, k, value int) {
-	s.values[(j*s.SubsetSize)+k] = value
+// Solved returns true if we have solved the puzzle, false otherwise.
+func (s *Sudoku) Solved() bool {
+	// Does every square have a value?
+	for _, gridSquare := range s.values {
+		if gridSquare.Value == 0 {
+			return false
+		}
+	}
+
+	return s.obeysConstraints()
 }
 
+func (s *Sudoku) obeysConstraints() bool {
+	for i := 0; i < s.SubsetSize; i++ {
+		if !s.Row(i).allValuesUnique() {
+			return false
+		}
+
+		if !s.Column(i).allValuesUnique() {
+			return false
+		}
+
+		if !s.Subgrid(i).allValuesUnique() {
+			return false
+		}
+	}
+
+	return true
+}
+
+// Row returns a Subset containing the squares contained by the
+// column at position k, indexed from 0 at the top to (size^2)-1 at the bottom.
 func (s *Sudoku) Row(j int) Subset {
 	return s.values[j*s.SubsetSize : (j+1)*s.SubsetSize]
 }
 
+// Column returns a Subset containing the squares contained by the
+// column at position k, indexed from 0 at the left to (size^2)-1 at the right.
 func (s *Sudoku) Column(k int) Subset {
-	col := make([]int, s.SubsetSize)
+	col := make(Subset, s.SubsetSize)
 	for j := 0; j < s.SubsetSize; j++ {
 		col[j] = s.values[k+(j*s.SubsetSize)]
 	}
@@ -58,8 +111,11 @@ func (s *Sudoku) Column(k int) Subset {
 	return col
 }
 
-func (s *Sudoku) Grid(l int) Subset {
-	grid := make([]int, s.SubsetSize)
+// Subgrid returns a Subset containing the grid squares contained by the
+// subgrid at position l, with 0 at the top left, (size^2)-1 at the bottom
+// right, left to right, top to bottom
+func (s *Sudoku) Subgrid(l int) Subset {
+	grid := make(Subset, s.SubsetSize)
 	offset := l % s.sizeFactor
 	startIndex := ((l - offset) * s.SubsetSize) + (offset * s.sizeFactor)
 	for gri := 0; gri < s.sizeFactor; gri++ {
@@ -69,4 +125,108 @@ func (s *Sudoku) Grid(l int) Subset {
 	}
 
 	return grid
+}
+
+// Set assigns the Value of the GridSquare at row j, column k to val.
+// It then removes val from the PossibleValues for every other square
+// within the same row, column, or gridsquare.
+func (s *Sudoku) Set(j, k, val int) error {
+	gridSquare := s.values[(j*s.SubsetSize)+k]
+
+	if _, ok := gridSquare.PossibleValues[val]; !ok {
+		pvs := make([]int, 0)
+		for v, _ := range gridSquare.PossibleValues {
+			pvs = append(pvs, v)
+		}
+
+		return fmt.Errorf("Value %d not in list of candidates %v", val, pvs)
+	}
+
+	// Set the value and set the possibles to only that value
+	gridSquare.Value = val
+	gridSquare.PossibleValues = map[int]struct{}{val: struct{}{}}
+
+	// Remove the value from the candidates for every other square in the same
+	// row, column, or subgrid
+	s.Row(gridSquare.RowIndex).MaskValue(k, val)
+	s.Column(gridSquare.ColumnIndex).MaskValue(j, val)
+	s.Subgrid(gridSquare.SubgridIndex).MaskValue(SquareIndexInSubgrid(j, k, s.sizeFactor), val)
+
+	return nil
+}
+
+// SubgridIndex returns the index of the subgrid containing the square at
+// row j, column k, given a sizexsize grid of sizexsize subgrids, with the
+// subgrids indexed starting with 0 at the top left, left to right, top to bottom.
+func SubgridIndex(j, k, size int) int {
+	return j - (j % size) + (k / size)
+}
+
+// SquareIndexInSubgrid returns the index of the square _within the subgrid_ -
+// indexed from 0 at top left to (size^2)-1 at the bottom right, left to right,
+// top to bottom.
+func SquareIndexInSubgrid(j, k, size int) int {
+	return ((j % size) * size) + (k % size)
+}
+
+// GridSquare is an individual square within the overall grid.  Each
+// Sudoku will contain n^4 squares (where the Sudoku is composed of n^2 nxn subgrids).
+type GridSquare struct {
+	Value          int
+	PossibleValues map[int]struct{}
+	RowIndex       int
+	ColumnIndex    int
+	SubgridIndex   int
+}
+
+// Constrain limits the candidate values for the square to those contained
+// by possibles
+func (gs *GridSquare) Constrain(possibles []int) {
+	candidates := make(map[int]struct{})
+	for _, pv := range possibles {
+		candidates[pv] = struct{}{}
+		// Go ahead and make sure it's in the stored list of possibles
+		gs.PossibleValues[pv] = struct{}{}
+	}
+
+	for v, _ := range gs.PossibleValues {
+		// If the value is not in the list of candidates passed in, delete it
+		if _, ok := candidates[v]; !ok {
+			delete(gs.PossibleValues, v)
+		}
+	}
+}
+
+// RemoveCandidate removes the value exclusion from PossibleValues
+func (gs *GridSquare) RemoveCandidate(exclusion int) {
+	delete(gs.PossibleValues, exclusion)
+}
+
+type Subset []*GridSquare
+
+// MaskValue removes val from the PossibleValues of every GridSquare in the
+// Subset other than the one at position i
+func (ss Subset) MaskValue(i, val int) {
+	for ssIndex := range ss {
+		if ssIndex != i {
+			// Delete the value from the candidate list
+			ss[ssIndex].RemoveCandidate(val)
+		}
+	}
+}
+
+func (ss Subset) allValuesUnique() bool {
+	ssVals := make(map[int]int)
+
+	for _, i := range ss {
+		if i.Value > 0 {
+			if ssVals[i.Value] > 0 {
+				return false
+			} else {
+				ssVals[i.Value] = 1
+			}
+		}
+	}
+
+	return true
 }
--- sudoku/sudoku_test.go
+++ sudoku/sudoku_test.go
@@ -11,81 +11,252 @@ import (
 
 var _ = Describe("Sudoku", func() {
 	var (
-		s        *sudoku.Sudoku
-		initRows [][]int
-		size     int
+		s          *sudoku.Sudoku
+		initRows   [][]int
+		size       int
+		subsetSize int
 	)
+
+	BeforeEach(func() {
+		s = nil
+	})
+
+	JustBeforeEach(func() {
+		subsetSize = int(math.Pow(float64(size), 2))
+	})
 
 	Describe("New", func() {
 		It("Returns a new correctly-sized Sudoku", func() {
 			for size := 2; size < 5; size++ {
-				sud := sudoku.New(size)
-				Expect(sud.SubsetSize).To(Equal(int(math.Pow(float64(size), 2))))
+				s = sudoku.New(size)
+				Expect(s.SubsetSize).To(Equal(int(math.Pow(float64(size), 2))))
+			}
+		})
+
+		It("Assigns the correct row indices", func() {
+			s = sudoku.New(3)
+			for j := 0; j < s.SubsetSize; j++ {
+				for k := 0; k < s.SubsetSize; k++ {
+					Expect(s.Row(j)[k].RowIndex).To(Equal(j))
+				}
 			}
 		})
 	})
 
-	BeforeEach(func() {
-		size = 3
-		initRows = [][]int{
-			{2, 7, 0, 4, 0, 8, 0, 0, 6},
-			{4, 0, 0, 0, 6, 0, 0, 0, 3},
-			{0, 9, 6, 7, 0, 2, 4, 0, 5},
-			{0, 6, 4, 5, 0, 0, 0, 0, 0},
-			{0, 0, 0, 9, 0, 0, 0, 0, 4},
-			{7, 1, 0, 6, 4, 3, 8, 5, 2},
-			{0, 0, 0, 0, 0, 5, 6, 0, 0},
-			{5, 0, 0, 0, 0, 6, 0, 3, 9},
-			{0, 2, 1, 0, 0, 4, 0, 0, 8},
-		}
-	})
-
-	JustBeforeEach(func() {
-		s = sudoku.NewFromRows(size, initRows)
-		Expect(s).NotTo(BeNil())
-	})
-
-	Describe("Row", func() {
+	Describe("SubgridIndex", func() {
 		var (
-			j int
+			subgridIndices [][]int
 		)
 
 		BeforeEach(func() {
-			j = 5
+			size = 3
+			subgridIndices = [][]int{
+				{0, 0, 0, 1, 1, 1, 2, 2, 2},
+				{0, 0, 0, 1, 1, 1, 2, 2, 2},
+				{0, 0, 0, 1, 1, 1, 2, 2, 2},
+				{3, 3, 3, 4, 4, 4, 5, 5, 5},
+				{3, 3, 3, 4, 4, 4, 5, 5, 5},
+				{3, 3, 3, 4, 4, 4, 5, 5, 5},
+				{6, 6, 6, 7, 7, 7, 8, 8, 8},
+				{6, 6, 6, 7, 7, 7, 8, 8, 8},
+				{6, 6, 6, 7, 7, 7, 8, 8, 8},
+			}
 		})
 
-		It("Returns the row at the specified index", func() {
-			Expect(s.Row(j)).To(ConsistOf(initRows[j]))
-		})
-	})
-
-	Describe("Column", func() {
-		var (
-			k int
-		)
-
-		BeforeEach(func() {
-			k = 3
-		})
-
-		It("Returns the column at the specified index", func() {
-			for j, val := range s.Column(k) {
-				Expect(val).To(Equal(initRows[j][k]))
+		It("Returns the correct subgrid index for the specified row and column", func() {
+			for j := 0; j < subsetSize; j++ {
+				for k := 0; k < subsetSize; k++ {
+					Expect(sudoku.SubgridIndex(j, k, size)).To(Equal(subgridIndices[j][k]))
+				}
 			}
 		})
 	})
 
-	Describe("Grid", func() {
+	Describe("SquareIndexInSubgrid", func() {
 		var (
-			l int
+			squareIndices [][]int
 		)
 
 		BeforeEach(func() {
-			l = 4
+			size = 3
+			squareIndices = [][]int{
+				{0, 1, 2, 0, 1, 2, 0, 1, 2},
+				{3, 4, 5, 3, 4, 5, 3, 4, 5},
+				{6, 7, 8, 6, 7, 8, 6, 7, 8},
+				{0, 1, 2, 0, 1, 2, 0, 1, 2},
+				{3, 4, 5, 3, 4, 5, 3, 4, 5},
+				{6, 7, 8, 6, 7, 8, 6, 7, 8},
+				{0, 1, 2, 0, 1, 2, 0, 1, 2},
+				{3, 4, 5, 3, 4, 5, 3, 4, 5},
+				{6, 7, 8, 6, 7, 8, 6, 7, 8},
+			}
 		})
 
-		It("Returns the lth grid left to right, top to bottom", func() {
-			Expect(s.Grid(l)).To(Equal(sudoku.Subset{5, 0, 0, 9, 0, 0, 6, 4, 3}))
+		It("Returns the correct index within the subgrid for the specified row and column", func() {
+			for j := 0; j < subsetSize; j++ {
+				for k := 0; k < subsetSize; k++ {
+					Expect(sudoku.SquareIndexInSubgrid(j, k, size)).To(Equal(squareIndices[j][k]))
+				}
+			}
+		})
+	})
+
+	Describe("Sudoku methods", func() {
+		BeforeEach(func() {
+			size = 3
+			initRows = [][]int{
+				{2, 7, 0, 4, 0, 8, 0, 0, 6},
+				{4, 0, 0, 0, 6, 0, 0, 0, 3},
+				{0, 9, 6, 7, 0, 2, 4, 0, 5},
+				{0, 6, 4, 5, 0, 0, 0, 0, 0},
+				{0, 0, 0, 9, 0, 0, 0, 0, 4},
+				{7, 1, 0, 6, 4, 3, 8, 5, 2},
+				{0, 0, 0, 0, 0, 5, 6, 0, 0},
+				{5, 0, 0, 0, 0, 6, 0, 3, 9},
+				{0, 2, 1, 0, 0, 4, 0, 0, 8},
+			}
+		})
+
+		JustBeforeEach(func() {
+			s = sudoku.NewFromRows(size, initRows)
+			Expect(s).NotTo(BeNil())
+		})
+
+		Describe("Solved", func() {
+
+		})
+
+		Describe("Row", func() {
+			var (
+				j int
+			)
+
+			BeforeEach(func() {
+				j = 5
+			})
+
+			It("Returns the row at the specified index", func() {
+				for k, gs := range s.Row(j) {
+					Expect(gs.Value).To(Equal(initRows[j][k]))
+				}
+			})
+		})
+
+		Describe("Column", func() {
+			var (
+				k int
+			)
+
+			BeforeEach(func() {
+				k = 3
+			})
+
+			It("Returns the column at the specified index", func() {
+				for j, val := range s.Column(k) {
+					Expect(val.Value).To(Equal(initRows[j][k]))
+				}
+			})
+		})
+
+		Describe("Subgrid", func() {
+			var (
+				l            int
+				expectedVals []int
+			)
+
+			BeforeEach(func() {
+				l = 4
+				expectedVals = []int{
+					5, 0, 0,
+					9, 0, 0,
+					6, 4, 3}
+			})
+
+			It("Returns the lth grid left to right, top to bottom", func() {
+				for i, gs := range s.Subgrid(l) {
+					Expect(gs.Value).To(Equal(expectedVals[i]))
+				}
+			})
+		})
+
+		Describe("Solved", func() {
+			When("There are still 0-valued squares", func() {
+				It("Returns false", func() {
+					Expect(s.Solved()).To(BeFalse())
+				})
+			})
+
+			When("There are duplicate values within a constrained subset (row/column/subgrid)", func() {
+				BeforeEach(func() {
+					initRows[2][0] = 2
+				})
+
+				It("Returns false", func() {
+					Expect(s.Solved()).To(BeFalse())
+				})
+			})
+
+			When("The puzzle has been solved", func() {
+				BeforeEach(func() {
+					initRows = [][]int{
+						{1, 9, 4, 2, 6, 8, 3, 7, 5},
+						{8, 5, 3, 9, 7, 4, 6, 1, 2},
+						{7, 2, 6, 3, 5, 1, 9, 8, 4},
+						{5, 1, 7, 8, 4, 3, 2, 9, 6},
+						{9, 4, 8, 1, 2, 6, 7, 5, 3},
+						{3, 6, 2, 7, 9, 5, 8, 4, 1},
+						{4, 7, 5, 6, 3, 9, 1, 2, 8},
+						{2, 3, 1, 5, 8, 7, 4, 6, 9},
+						{6, 8, 9, 4, 1, 2, 5, 3, 7},
+					}
+				})
+
+				It("Returns true", func() {
+					Expect(s.Solved()).To(BeTrue())
+				})
+			})
+		})
+
+		Describe("Set", func() {
+			var (
+				j, k, val int
+			)
+
+			BeforeEach(func() {
+				j = 3
+				k = 4
+				val = 8
+			})
+
+			JustBeforeEach(func() {
+				Expect(s.Row(j)[k].Value).NotTo(Equal(val))
+			})
+
+			It("Sets the specified square to contain the specified value", func() {
+				s.Set(j, k, val)
+				Expect(s.Row(j)[k].Value).To(Equal(val))
+			})
+
+			It("Does not return an error", func() {
+				Expect(s.Set(j, k, val)).NotTo(HaveOccurred())
+			})
+
+			When("The value is not in the list of candidates for the square", func() {
+				JustBeforeEach(func() {
+					s.Row(j)[k].RemoveCandidate(val)
+				})
+
+				It("Returns an error", func() {
+					Expect(s.Set(j, k, val)).To(HaveOccurred())
+				})
+
+				It("Does not modify the value", func() {
+					prev := s.Row(j)[k].Value
+					s.Set(j, k, val)
+					Expect(s.Row(j)[k].Value).To(Equal(prev))
+					Expect(s.Row(j)[k].Value).NotTo(Equal(val))
+				})
+			})
 		})
 	})
 })
